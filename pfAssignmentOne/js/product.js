5afd67fee2e2c8782a59c53ebec6424d481a7c21
zygoh1986 <69612872+zygoh1986@users.noreply.github.com>
2020-10-13 17:54:14 +0800

    13/10/20

--- pfAssignmentOne/js/product.js
+++ pfAssignmentOne/js/product.js
@@ -1,1 +1,51 @@
+function getMyServiceCost(){
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	} else {
+  		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	xmlhttp.onreadystatechange = function(){
+  		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    		var jsondata = JSON.parse(xmlhttp.responseText); //retrieve result as an JavaScript object
+   			var catalog = jsondata.catalog;
+			var output='<form>';
+			output+='<select id="service_select" onchange="serviceSelect()" >';
+   			for (var i=0; i < catalog.length; i++){
+    			output +='<option value="'+i+'">'+catalog[i].title+'</option>';
+   			}
+			output+='</select>';
+			output+='</form>';
+   			document.getElementById("servicedetails").innerHTML=output;
+    	}
+  	}
+	xmlhttp.open("GET","product.json",true);
+	xmlhttp.send();
+}
 
+function serviceSelect(){
+	var selectBox = document.getElementById("service_select");
+	var serviceIndex = selectBox.options[selectBox.selectedIndex].value;
+	getServiceInfo(serviceIndex);
+}
+function getServiceInfo(i){
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	} else {
+  		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+	xmlhttp.onreadystatechange = function(){
+  		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    		var jsondata = JSON.parse(xmlhttp.responseText); //retrieve result as an JavaScript object
+   			var catalog = jsondata.catalog;
+			var output='';
+   			output+= '<li>SGD: '+catalog[i].SG+'</li>';
+			output+= '<li>INR: '+catalog[i].IN+'</li>';
+			output+= '<li>USD: '+catalog[i].US+'</li>';
+   			document.getElementById("servicecost").innerHTML=output;
+    	}
+  	}
+	xmlhttp.open("GET","product.json",true);
+	xmlhttp.send();
+}
